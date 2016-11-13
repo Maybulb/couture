6c81fc193cead896fe29790f431a105c2636e626
Josh Trommel <joshtrommel@gmail.com>
2016-11-12 21:19:11 -0800

    Ethan Saves Lives :100:

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const electron = require('electron')
 	, app = electron.app
 	, BrowserWindow = electron.BrowserWindow
-	, ipc = electron.ipcMain;
+	, ipcMain = electron.ipcMain;
 
 app.on('ready', function() {
 	var mainWindow = new BrowserWindow({
@@ -9,9 +9,15 @@ app.on('ready', function() {
 		resizable: true
 	});
 
+	mainWindow.openDevTools();
+
 	mainWindow.loadURL('file://' + __dirname + '/public/index.html');
 
 	mainWindow.on('closed', function() {
 		mainWindow = null
 	});
 });
+
+ipcMain.on("submit", function (e, style, gender) {
+	console.log(style + " " + gender);
+})
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,8 @@
 // nothing yet
+
+const {ipcRenderer} = require('electron')
+const $ = require("jquery");
+
+function submit() {
+	ipcRenderer.send("submit", $("#style option:selected").text(), $("#gender option:selected").text())
+}
