f83609fad1318ac634d3bf2f133fe833194ce5bf
Josh Trommel <joshtrommel@gmail.com>
2016-11-12 20:59:14 -0800

    Sass with Gulp :100:

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp')
 	, sass = require('gulp-sass');
 
-gulp.task('default', function() {
-	console.log('i am gonna yell');
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function() {
+	return gulp.src('public/css/style.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('public/css'))
 });
